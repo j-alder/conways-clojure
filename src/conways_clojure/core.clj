1d82e056d43729b4df9b4e0fb80e1f8de6591e75
j-alder <jonathon.alder@gmail.com>
2022-04-14 21:09:15 -0400

    implement quil; fix output

--- src/conways_clojure/core.clj
+++ src/conways_clojure/core.clj
@@ -1,37 +1,38 @@
 (ns conways-clojure.core
-  (:use seesaw.core)
-  (:use clojure.pprint))
+  (:use clojure.pprint)
+  (:require [quil.core :as q :include-macros true])
+  (:require [quil.middleware :as m]))
 
-;Any live cell with two or three live neighbours survives.
-;Any dead cell with three live neighbours becomes a live cell.
-;All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+;; length of each side of state
+(def side-length 40)
 
-; create the starting board with initial live cell coordinates
-; set live cells to true, dead cells to false
-(defn initial-board [coords]
-  (vec
-    (for [i (range 10)]
-      (vec
-        (for [j (range 10)]
-          (if (contains? coords [i j]) true false))))))
+;; vector of random t/f values
+(defn random-row []
+  (vec (repeatedly side-length #(= (rand-int 2) 0))))
 
-; get number of live cells in a collection
+;; initial (randomized) state
+(def state
+  (vec (repeatedly side-length random-row)))
+
+;; get number of live cells in a collection
 (defn count-live [neighbors]
   (count (filter true? neighbors)))
 
-; apply rules to individual cell and mutate appropriately
-(defn determine-fate [cell neighbors]
+;; apply rules to individual cell and return true or false
+;; depending on outcome
+(defn evolve-cell [cell neighbors]
   (let [live-neighbors (count-live neighbors)]
-    (if (= cell true)
-      ; any live cell with two or three live neighbors survives (else dies)
+    (if cell
+      ; any live cell with two or three live neighbors stays alive (else dies)
       (or (= live-neighbors 2) (= live-neighbors 3))
-      ; any dead cell with three live neighbors reanimates (else stays dead)
+      ; any dead cell with three live neighbors becomes alive (else stays dead)
       (or (= live-neighbors 3)))))
 
-; get the value of a cell at a given coordinate in a board
+;; get the value of a cell at a given coordinate in a board
 (defn get-cell [board i j]
   (nth (nth board i []) j false))
 
+;; get all neighbors of a cell at coordinates i, j
 (defn get-neighbors [board i j]
   (vector
     (get-cell board (- i 1) (- j 1))
@@ -43,23 +44,41 @@
     (get-cell board (+ i 1) j)
     (get-cell board (+ i 1) (+ j 1))))
 
-(defn evolve [board]
-  (vec
-    (for [i (range (count board))]
-      (vec
-        (for [j (range (count (nth board i)))]
-          (determine-fate (get-cell board i j) (get-neighbors board i j)))))))
+;; cycle one evolution of the board, calculating liveliness of each cell
+(defn evolve-state [state]
+  (vec (for [i (range (count state))]
+         (vec (for [j (range (count (nth state i)))]
+                (evolve-cell (get-cell state i j) (get-neighbors state i j)))))))
 
-(defn -main [& args]
-  (let [b (initial-board #{[3 3] [2 2] [3 2] [5 5] [5 4] [5 3]})]
-    (pprint b)
-    (let [c (evolve b)]
-      (pprint c))))
+(defn multiplier [idx]
+  (int (/ idx side-length)))
 
-;(defn -main [& args]
-;  (invoke-later
-;    (-> (frame :title "Conway's Clojure",
-;               :content content,
-;               :on-close :exit)
-;        pack!
-;        show!)))
+(defn coordinate [cell-size idx]
+  (* cell-size (- idx (* (multiplier idx) side-length))))
+
+;; draw the initial state
+(defn draw-state [state]
+  (q/background 240)
+  (let [c-size (/ (q/width) side-length)]
+    (doseq [[row-idx row] (map-indexed vector state)]
+      (let [x (coordinate c-size row-idx)]
+        (doseq [[col-idx cell] (map-indexed vector row)]
+          (let [y (coordinate c-size col-idx)]
+            (q/fill (if cell 0 255))
+            (q/rect x y c-size c-size)))))))
+
+(defn setup []
+  (q/frame-rate 10)
+  (q/color-mode :hsb)
+  (q/no-stroke)
+  state)
+
+(q/defsketch conways-clojure.core
+             :host "host"
+             :size [500 500]
+             :setup setup
+             :update evolve-state
+             :draw draw-state
+             :middleware [m/fun-mode])
+
+(defn -main [& args])
